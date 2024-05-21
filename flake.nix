24bee4674f81e627a0546be8918712dc6b6ff3de
Gabriel Hoshino <gyhoshino@gmail.com>
2024-05-21 14:37:12 -0500

    changing to unstable package repo

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My Overlay for Custom Packages";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
   outputs = {
--- pkgs/emacs/flake.nix
+++ pkgs/emacs/flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs = {
-      url = github:nixos/nixpkgs?ref=nixos-23.05;
+      url = github:nixos/nixpkgs?ref=nixos-unstable;
     };
   };
 
